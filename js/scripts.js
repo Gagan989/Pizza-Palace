a33d6e812b6ed08718d471022b06e5e284ab949c
gagan chhabra <gsgagan85@gmail.com>
2021-04-28 12:23:03 -0400

    Add readme file and licence file

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,28 @@
+// business logic
+
 function pizzaOrder (name,size,crust,topping)
 {
-  this.name =name;
-  this.size =size;
+  this.name = name;
+  this.size = size;
   this.crust = crust;
   this.topping = topping;
 }
 
-pizzaOrder.prototype.total=function(){
-    return this.size+this.crust+this.topping;
+pizzaOrder.prototype.total= function(){
+  return this.size+this.crust+this.topping;
 
 }
 
-$(document).ready(function(event)) {
+
+// user logic
+
+$(document).ready(function(){
+
+
+
+  $("form#neworder").submit(function(event) {
     event.preventDefault();
-}
+
 
     var pizzaType;
     var sizePrice = 0;
@@ -23,35 +32,41 @@ $(document).ready(function(event)) {
     var crustName =' ';
     var toppingsName = ' ';
 
+  //to get pizza type names
     function pizzaTypeValue(){
-        if($("#Modal1").is(':visible')){
-          pizzaType = 'BBQ Steak';
-        }else if ($("#Modal2").is(':visible')) {
-          pizzaType = "Chicken & Beef";
-  
-        }else if ($("#Modal3").is(':visible')) {
-          pizzaType = "Chcken Tikka";
-  
-        }else if ($("#Modal4").is(':visible')) {
-          pizzaType = "Meat Lovers";
-  
-        }else if ($("#Modal5").is(':visible')) {
-          pizzaType = "Vegetarian";
-  
-        }else if ($("#Modal6").is(':visible')) {
-          pizzaType = "Cheese";
-  
-        }else if ($("#Modal7").is(':visible')) {
-          pizzaType = "Hawaiian";
-  
-        }else if ($("#Modal8").is(':visible')) {
-          pizzaType = "Combination";
-  
-        }
-    }  
-    pizzaTypeValue();
-    
-    function pizzaSizeValue(){
+      if($("#Modal1").is(':visible')){
+        pizzaType = 'BBQ Steak';
+      }else if ($("#Modal2").is(':visible')) {
+        pizzaType = "Chicken & Beef";
+
+      }else if ($("#Modal3").is(':visible')) {
+        pizzaType = "Chicken Tikka";
+
+      }else if ($("#Modal4").is(':visible')) {
+        pizzaType = "Meat Lovers";
+
+      }else if ($("#Modal5").is(':visible')) {
+        pizzaType = "Vegetarian";
+
+      }else if ($("#Modal6").is(':visible')) {
+        pizzaType = "Cheese";
+
+      }else if ($("#Modal7").is(':visible')) {
+        pizzaType = "Hawaiian";
+
+      }else if ($("#Modal8").is(':visible')) {
+        pizzaType = "Combination";
+
+      }
+
+
+
+  }
+  pizzaTypeValue();
+
+  // to get  price per size and size name
+
+  function pizzaSizeValue(){
         for(var i = 0;i<document.getElementsByName('pizzasize').length;i++){
           if(document.getElementsByName('pizzasize')[i].checked){
              sizePrice = parseInt(document.getElementsByName('pizzasize')[i].value);
@@ -62,46 +77,59 @@ $(document).ready(function(event)) {
 
     pizzaSizeValue();
 
-    function pizzaCrustValue(){
-        for(var i = 0;i<document.getElementsByName('pizzacrust').length;i++){
-          if(document.getElementsByName('pizzacrust')[i].checked){
-             crustPrice = parseInt(document.getElementsByName('pizzacrust')[i].value);
-             crustName = document.getElementsByName('pizzacrust')[i].id;
-          }
+
+// to get crust name and Value
+  function pizzaCrustValue(){
+      for(var i = 0;i<document.getElementsByName('pizzacrust').length;i++){
+        if(document.getElementsByName('pizzacrust')[i].checked){
+           crustPrice = parseInt(document.getElementsByName('pizzacrust')[i].value);
+           crustName = document.getElementsByName('pizzacrust')[i].id;
         }
-    }
-    pizzaCrustValue();
+      }
+  }
 
-    function toppingPriceTotal(){
-        for(var i = 0;i<document.getElementsByName('top_ings').length;i++){
-          if(document.getElementsByName('top_ings')[i].checked){
-            toppingPrice = toppingPrice+ parseInt(document.getElementsByName('top_ings')[i].value);
- 
-            toppingsName = toppingsName + document.getElementsByName('top_ings')[i].id + "," ;
-          }
-        }
-    }
- 
-    toppingPriceTotal();
+  pizzaCrustValue();
 
-    var  newPizzaOrder = new pizzaOrder(
-        pizzaType,sizePrice,crustPrice,toppingPrice
-    );
+  //to get  total toppingPrice and names of selected Toppings
+  function toppingPriceTotal(){
+       for(var i = 0;i<document.getElementsByName('top_ings').length;i++){
+         if(document.getElementsByName('top_ings')[i].checked){
+           toppingPrice = toppingPrice+ parseInt(document.getElementsByName('top_ings')[i].value);
 
-$("p#total").text("TOTAL: "+newPizzaOrder.total()+"ksh");
+           toppingsName = toppingsName + document.getElementsByName('top_ings')[i].id + "," ;
+         }
+       }
+   }
 
-$("input[name='delivery']").on("click", function(){
+   toppingPriceTotal();
+
+ //object
+   var  newPizzaOrder = new pizzaOrder(
+    pizzaType,sizePrice,crustPrice,toppingPrice
+  );
+
+  // toggle delivery form
+  $("p#total").text("TOTAL: "+newPizzaOrder.total()+"ksh");
+
+  $("input[name='delivery']").on("click", function(){
     $(".delivery-form1").toggle(this.value === "false" && this.checked);
   });
 
-  $("form.delivery-form").submit(function(event){
-    event.preventDefault();
-    var fullname = $("#new-full-name").val();
-    var location = $("#new-location").val();
-    alert(fullname + " your Order will be delivered to "+location);
-  })
+  ///alert message from input values ~ delivery form
+     var fullname = "";
+     var location = "";
 
-  $("div#Orders").append("<p><span class='neworder'> Pizza Type: "+ newPizzaOrder.name + " - "+sizeName+ "</span></p><br>" +
+
+    $("form.delivery-form").submit(function(event){
+      event.preventDefault();
+       fullname = $("#new-full-name").val();
+       location = $("#new-location").val();
+      alert(fullname + " your Order will be delivered to "+location);
+    })
+
+
+    //output to order summary
+    $("div#Orders").append("<p><span class='neworder'> Pizza Type: "+ newPizzaOrder.name + " - "+sizeName+ "</span></p><br>" +
                            "<p><span class='neworder'> Toppings: "+ toppingsName +"</span></p><br> " +
                            "<p><span class='neworder'> Crust: "+ crustName +"</span></p><br>" +
                            "<p><span class='neworder totalorder' > Total in ksh : "+ newPizzaOrder.total()+ "</span></p><br><br>"
@@ -109,19 +137,30 @@ $("input[name='delivery']").on("click", function(){
 
     var pizzaGrandTotal = 0;
     for(var i = 0;i<$('.totalorder').length;i++){
-        pizzaGrandTotal  += newPizzaOrder.total();
-        $("div#totalOrder").text("Grand Total = "+pizzaGrandTotal+"ksh");
+      pizzaGrandTotal  += newPizzaOrder.total();
+      $("div#totalOrder").text("Grand Total = "+pizzaGrandTotal+"ksh");
+
+
     }
 
     $(".confirm2").click(function(event){
-        alert("Thank-You for ordering with us.Your order will be ready shortly");
-        alert($("div#totalOrder").text());
-      })
-  
+      alert("Thank-You for ordering with us.Your order will be ready shortly");
+      alert($("div#totalOrder").text());
     })
 
-)};
 
 
+  })
+
+
+
+
+
+
+
+  ///to make order summary to move when screen is scrolled
+  // $(window).scroll(function(){
+  // $(".ordersummary").css({"margin-top": ($(window).scrollTop()) + "px"});
+});
 
         
---
+++ readme.md
@@ -0,0 +1,27 @@
+# Pizza Palace
+
+## Description
+This is an online pizza resturant website offering a wide range of pizza's according to your specifications
+
+## Author 
+Gagan Chhabra
+
+## Setup/Installation Requirements
+Inorder to acces the project in your local files, you can clone it in these simple steps:
+
+1. Open your terminal
+2. use this command to clone '$ git clone https://github.com/Gagan989/Pizza-Palace.git
+3. This will clone the repository in your local folder
+4. Run the code using google chrome
+
+## Technologies Used
+* HTML
+* CSS
+* JavaScript
+* Bootstrap
+
+## Contact details
+gsgagan85@gmail.com
+
+### License
+This is under the [MIT](license) license
